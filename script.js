9727981e678c7c01d79f06033f6a9719e17527ff
“CJ” <1592105+cjluzroque@users.noreply.github.com>
2024-11-01 01:42:31 -0400

    Create 16x16 grid and boxes changes from green to pink

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 // Script for Etch-a-Sketch
+
+const grid = document.querySelector('#container');
+
+//Make container attribute flex column 
+
+// Create Row 
+for (i = 0; i < 16; i++) {
+    // Make a div to append each box to be a row 
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    // Create 16 divs in row
+    for (j = 0; j < 16; j++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.classList.add("noHover");
+
+        row.appendChild(box);
+    }
+
+// Append to container 
+grid.appendChild(row);
+}
+
+
+const targets = document.querySelectorAll('.box')
+//Event listener for hover action
+targets.forEach(target => {
+    target.addEventListener("mouseover", function () {
+
+        target.addEventListener("mouseover", function () {
+        //set attribute of the box to a different color 
+        // adds class "hover" to your new div
+        target.classList.add("hover");
+
+        // removes "noHover" class from div
+        target.classList.remove("noHover");
+
+        });
+    });
+});
